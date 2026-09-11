68c48d1e07a37fc4070742f92feb7a0fe39e0c64
wadaiiiii <andi.seppewali@unsulbar.ac.id>
2026-09-11 22:38:20 +0800

    Add Word learning module page

---
+++ src/pages/Word.jsx
@@ -0,0 +1,8 @@
+export default function Word(){
+ return <section>
+ <h1>Microsoft Word Mastery</h1>
+ <p>Materi: dokumen profesional, template, styles, dan mail merge.</p>
+ <h3>Latihan</h3>
+ <ul><li>Membuat surat resmi</li><li>Membuat template</li><li>Simulasi mail merge</li></ul>
+ </section>
+}
