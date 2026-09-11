4bbf8ccb702672a6d7c6f6fe75e49cab3914a993
wadaiiiii <andi.seppewali@unsulbar.ac.id>
2026-09-11 22:38:48 +0800

    Add LMS navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,3 @@
+export default function Navbar(){
+ return <header style={{padding:'15px',borderBottom:'1px solid #ddd'}}><b>Mastering Office Tools</b><span style={{float:'right'}}>FK UNSULBAR</span></header>
+}
