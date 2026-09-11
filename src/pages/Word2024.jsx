b059452cbc467327795b73b850f27f18b7e557e8
wadaiiiii <andi.seppewali@unsulbar.ac.id>
2026-09-11 23:18:55 +0800

    Add Word 2024 material page

---
+++ src/pages/Word2024.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Word2024(){
+ return <main style={{padding:40}}>
+  <h1>Microsoft Word 2024</h1>
+  <p>Materi pengolahan dokumen profesional.</p>
+  <h3>Daftar Materi</h3>
+  <ul>
+   <li>Pengenalan Word 2024</li>
+   <li>Template Dokumen</li>
+   <li>Styles dan Heading</li>
+   <li>Mail Merge</li>
+   <li>Daftar Isi Otomatis</li>
+  </ul>
+ </main>
+}
