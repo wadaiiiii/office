97a624189c29be55ac578b23fca1498cc60c5829
wadaiiiii <andi.seppewali@unsulbar.ac.id>
2026-09-11 23:18:09 +0800

    Add material search component

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function SearchBox(){
+ return <input placeholder="Cari materi Office 2024..." style={{width:'100%',padding:16,borderRadius:14,border:'1px solid #ddd',fontSize:16}}/>;
+}
