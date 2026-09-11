b715bd4038968f95a7cf95ddd62a8db9c6890932
wadaiiiii <andi.seppewali@unsulbar.ac.id>
2026-09-11 22:37:14 +0800

    Add training module data

---
+++ src/data/modules.js
@@ -0,0 +1,6 @@
+export const modules=[
+ {id:1,title:'Microsoft Word Mastery',progress:80},
+ {id:2,title:'Microsoft Excel Productivity',progress:60},
+ {id:3,title:'Digital Forms Creation',progress:50},
+ {id:4,title:'Data Integration',progress:30}
+];
