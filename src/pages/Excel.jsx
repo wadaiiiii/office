dd170405292ba76aeb32936f07f703705aa88162
wadaiiiii <andi.seppewali@unsulbar.ac.id>
2026-09-11 22:38:24 +0800

    Add Excel learning module page

---
+++ src/pages/Excel.jsx
@@ -0,0 +1,8 @@
+export default function Excel(){
+ return <section>
+ <h1>Microsoft Excel Productivity</h1>
+ <p>Formula, lookup, pivot table, dan dashboard administrasi.</p>
+ <h3>Latihan</h3>
+ <ul><li>IF Function</li><li>XLOOKUP</li><li>Pivot Table</li></ul>
+ </section>
+}
