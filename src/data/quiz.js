98af8585f86f1b7b1ef7340f490f25afdded8d30
wadaiiiii <andi.seppewali@unsulbar.ac.id>
2026-09-11 22:38:57 +0800

    Add initial quiz bank

---
+++ src/data/quiz.js
@@ -0,0 +1,4 @@
+export const quiz=[
+{question:'Fungsi XLOOKUP digunakan untuk?',options:['Menghitung','Mencari data','Membuat grafik'],answer:'Mencari data'},
+{question:'Mail Merge digunakan untuk?',options:['Surat massal','Grafik','Database'],answer:'Surat massal'}
+]
